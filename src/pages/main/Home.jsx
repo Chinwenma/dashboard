5593ee9c722abeeecd06a7cf028353b15c649522
Chinwenma <okoriechinwe1327@gmail.com>
2023-04-23 20:07:36 -0700

    nav

--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -1,10 +1,13 @@
 import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
 import "./home.scss"
 export default function Home(){
     return(
         <div className="home">
         <Sidebar />
-        <div className="homeContainer">Container</div>
+        <div className="homeContainer">
+        <Navbar />
+        Home Container</div>
         </div>
     )
 }
